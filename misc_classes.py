a6b2b633dd21535f44f3a4999020ee196cd84164
Kevin_OVI <kevinsciences@gmail.com>
2022-07-09 21:13:39 +0200

    Correction d'une erreur critique sur le système de confirmation, qui faisait que les clics sur les boutons de confirmation s'effectuaient sur la dernière confirmation demandée

--- misc_classes.py
+++ misc_classes.py
@@ -38,18 +38,16 @@ class WelcomeMessagesCog(commands.Cog):
 
 
 class ConfirmationView(ui.View):
-	view_name = f"{bot_name}:confirmation"
-
 	def __init__(self):
 		super().__init__(timeout=30)
 		self.value = None
 
-	@ui.button(label="Confirmer", emoji="✅", style=nextcord.ButtonStyle.green, custom_id=f"{view_name}:confirm")
+	@ui.button(label="Confirmer", emoji="✅", style=nextcord.ButtonStyle.green)
 	async def yes_button(self, button, interaction):
 		self.value = True
 		self.stop()
 
-	@ui.button(label="Annuler", emoji="❌", style=nextcord.ButtonStyle.gray, custom_id=f"{view_name}:cancel")
+	@ui.button(label="Annuler", emoji="❌", style=nextcord.ButtonStyle.gray)
 	async def no_button(self, button, interaction):
 		self.value = False
 		self.stop()
